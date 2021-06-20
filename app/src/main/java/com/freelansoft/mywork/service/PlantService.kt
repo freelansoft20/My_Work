dc244de943c7dfb48caf083b63c0721d93354b2f
Moustafa <freelansoft20@gmail.com>
2021-06-20 12:23:22 +0200

    GPS with Live Data and MVVM correction

--- app/src/main/java/com/freelansoft/mywork/service/PlantService.kt
+++ app/src/main/java/com/freelansoft/mywork/service/PlantService.kt
@@ -9,7 +9,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PlantService(application: Application) {
+class PlantService {
 
      fun fetchPlants(plantName: String): MutableLiveData<ArrayList<Plant>> {
         var _plants = MutableLiveData<ArrayList<Plant>>()
